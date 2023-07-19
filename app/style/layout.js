ef38502982757e7c5fb3aa3e2322694788a391df
nethen <72537484+nethen@users.noreply.github.com>
2023-07-18 18:45:35 -0700

    Removed mention of inter

--- app/style/layout.js
+++ app/style/layout.js
@@ -2,6 +2,6 @@ import '../globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <article className={inter.className}>{children}</article>
+    <article>{children}</article>
   )
 }
